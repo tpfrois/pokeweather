199df4272319ef6f512335c0735f0daf610f883c
Thiago Frois <zthiagopf@gmail.com>
2021-06-30 12:54:24 -0300

    fix PokeCard infinite img loop

--- src/components/PokeCard/index.js
+++ src/components/PokeCard/index.js
@@ -5,29 +5,32 @@ import { RiSwordFill, RiRunFill } from 'react-icons/ri';
 import Pokeball from '../../assets/pokeball.png';
 import { Container, Stats, StatsBar } from './styles';
 
-const PokeCard = ({ img, type, name, attack, hp, defense, speed }) => (
-  <Container type={type}>
-    <img src={Pokeball} onError={(e) => e.target.src = Pokeball} onLoad={(e) => e.target.src = img} alt="Squirtle" />
-    <p>{name}</p>
-    <Stats>
-      <StatsBar skill="attack" level={attack} title={`${attack} attack`}>
-        <RiSwordFill size={18} color={'white'} />
-        <section><div></div></section>
-      </StatsBar>
-      <StatsBar skill="hp" level={hp} title={`${hp} hp`}>
-        <BsFillHeartFill size={18} color={'white'} />
-        <section><div></div></section>
-      </StatsBar>
-      <StatsBar skill="defense" level={defense} title={`${defense} defense`}>
-        <BsShieldShaded size={18} color={'white'} />
-        <section><div></div></section>
-      </StatsBar>
-      <StatsBar skill="speed" level={speed} title={`${speed} speed`}>
-        <RiRunFill size={18} color={'white'} />
-        <section><div></div></section>
-      </StatsBar>
-    </Stats>
-  </Container>
-);
+const PokeCard = ({ img, type, name, attack, hp, defense, speed }) => {
+
+  return (
+    <Container type={type}>
+      <img src={img ? img : Pokeball} alt={name} />
+      <p>{name}</p>
+      <Stats>
+        <StatsBar skill="attack" level={attack} title={`${attack} attack`}>
+          <RiSwordFill size={18} color={'white'} />
+          <section><div></div></section>
+        </StatsBar>
+        <StatsBar skill="hp" level={hp} title={`${hp} hp`}>
+          <BsFillHeartFill size={18} color={'white'} />
+          <section><div></div></section>
+        </StatsBar>
+        <StatsBar skill="defense" level={defense} title={`${defense} defense`}>
+          <BsShieldShaded size={18} color={'white'} />
+          <section><div></div></section>
+        </StatsBar>
+        <StatsBar skill="speed" level={speed} title={`${speed} speed`}>
+          <RiRunFill size={18} color={'white'} />
+          <section><div></div></section>
+        </StatsBar>
+      </Stats>
+    </Container>
+  )
+};
 
 export default PokeCard;
